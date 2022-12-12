1ff0b681a92049a6b8f7477224fc000a47df4eea
Juan Carlos Calatayud Miranda <40042640+jccm17@users.noreply.github.com>
2022-12-12 00:08:21 -0400

    update function delete files

--- src/main/java/com/example/restapi/springbootapp/controller/v1/CifradoController.java
+++ src/main/java/com/example/restapi/springbootapp/controller/v1/CifradoController.java
@@ -17,24 +17,18 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 import org.apache.logging.log4j.Logger;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -42,9 +36,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -53,9 +45,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
-import javax.sound.sampled.Line;
 import javax.validation.Valid;
 
 /**
--- src/main/java/com/example/restapi/springbootapp/utils/Uploads.java
+++ src/main/java/com/example/restapi/springbootapp/utils/Uploads.java
@@ -12,10 +12,12 @@ public class Uploads {
     public void eliminar(String file) {
 
         File fichero = new File(file);
-        if (fichero.delete())
-            System.out.println("file delete success");
-        else
-            System.out.println("fail delete file");
+        if (fichero.exists()) {
+            if (fichero.delete())
+                System.out.println("file delete success");
+            else
+                System.out.println("fail delete file");
+        }
     }
 
     public void eliminarTodo(String folder) {
