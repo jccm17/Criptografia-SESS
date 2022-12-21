e63216d475924c94ce316f37542e86220437ea23
Juan Carlos Calatayud Miranda <40042640+jccm17@users.noreply.github.com>
2022-12-21 16:46:30 -0400

    update encoding text respons

--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -2,27 +2,38 @@ package com.example.config;
 
 import java.io.File;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  *
  * @author Jccm.17
  */
 @Configuration
 @EnableWebMvc
-public class MvcConfig extends WebMvcConfigurerAdapter{
+public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-          .addResourceHandler("/resources/**")
-          .addResourceLocations("/resources/"); 
-    }  
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Bean
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
     }
 }
